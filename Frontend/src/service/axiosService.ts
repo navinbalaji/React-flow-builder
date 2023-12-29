50b10e24f327a61c14c7ec33a32bb9e9effa0c96
navinbalaji <naveenbalaji1199@gmail.com>
2023-12-29 22:24:43 +0530

    baseurl added

--- Frontend/src/service/axiosService.ts
+++ Frontend/src/service/axiosService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.BASE_URL || 'http://localhost:8000',
+    baseURL: import.meta.env.BASE_URL || "https://react-flow-builder.onrender.com",
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
